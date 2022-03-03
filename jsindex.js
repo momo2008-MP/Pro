20311e26c83f079f5b2fdf34f3c1fb35b59ef57f
momo2008-MP <86022212+momo2008-MP@users.noreply.github.com>
2022-03-03 09:06:52 +0200

    Add files via upload

---
+++ jsindex.js
@@ -0,0 +1,29 @@
+let telalogin0=document.getElementById('login');
+let telalogin1=document.getElementById('cc')
+
+let borda=document.getElementsByClassName('op')
+
+function mostrarcc(){
+    borda[0].style.border="none"
+borda[1].style.border="2px solid red"
+telalogin0.style.display="none"
+telalogin1.style.display="block"
+
+
+
+   
+    
+    
+    
+
+}
+function eu(){
+    telalogin0.style.display="block"
+    telalogin1.style.display="none"
+    borda[0].style.border="2px solid red"
+    borda[1].style.border="none"
+    
+
+}
+
+
